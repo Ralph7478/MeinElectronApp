608fea869092226fdb511acf57c15639e1844f6e
Ralph7478 <Ralph7874@gmx.de>
2025-06-18 15:18:07 +0200

    Initial commit

---
+++ main/main.ts
@@ -0,0 +1,27 @@
+//main/main.ts
+import { app, BrowserWindow } from 'electron';
+import * as path from 'path';
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      // Nur aktivieren wenn wirklich preload.js existiert:
+      preload: path.join(__dirname, 'preload.js'),
+      contextIsolation: true,
+    },
+  });
+
+  if (process.env.NODE_ENV === 'development') {
+    win.loadURL('http://localhost:5173/');
+  } else {
+    win.loadFile(path.join(__dirname, '../renderer/index.html'));
+  }
+};
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
